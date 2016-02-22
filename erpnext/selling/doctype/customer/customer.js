1faf72de0c85359609031ebd5730a906eb7c27b0
khatrijitendra <jitendra.k@indictranstech.com>
2016-02-22 18:45:30 +0530

    [Include custom_script_customer.js]

--- erpnext/selling/doctype/customer/customer.js
+++ erpnext/selling/doctype/customer/customer.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
+{% include 'customer_info/customer_info/custom_script_customer.js' %};
 
 frappe.ui.form.on("Customer", {
 	refresh: function(frm) {
